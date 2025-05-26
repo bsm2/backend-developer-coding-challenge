9ea4bbef88600a54427f575077b15b535866befd
bsm2 <basmatalaat2@gmail.com>
2025-05-26 23:54:01 +0300

    refactor: add calender style

--- resources/views/livewire/posts/calendar.blade.php
+++ resources/views/livewire/posts/calendar.blade.php
@@ -1,5 +1,17 @@
-<div>
-    <div id="calendar" class="text-white"></div>
+<div class="py-16">
+    <div class="max-w-10xl mx-auto sm:px-6 lg:px-20">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div>
+                    <div>
+                        <div id="calendar" class="text-white"></div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>
 
 @script()
@@ -16,7 +28,8 @@
                 },
                 events: async function(info, successCallback, failureCallback) {
                     try {
-                        const response = await fetch(`/calendar-events?start=${info.startStr}&end=${info.endStr}`);
+                        const response = await fetch(
+                            `/calendar-events?start=${info.startStr}&end=${info.endStr}`);
                         const result = await response.json();
                         successCallback(result.data);
                     } catch (error) {
